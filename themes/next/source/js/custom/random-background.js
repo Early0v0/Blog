616c83120268ffe1b51fb95a3a13cc6344ad5d04
Early <lin_erli@outlook.com>
2020-04-02 16:36:12 +0800

    Change Random-background

--- themes/next/source/js/custom/random-background.js
+++ themes/next/source/js/custom/random-background.js
@@ -1,10 +1,3 @@
-var imgArr = ["/images/background1.webp", "/images/background2.webp"];
-var imgArrDark = ["/images/background-dark1.webp", "/images/background-dark2.webp", "/images/background-dark3.webp"];
-let darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-if (darkMode) {
-  var rand = Math.floor(Math.random() * imgArrDark.length);
-  document.body.style.backgroundImage = "url(" + imgArrDark[rand] + ')';
-} else {
-  var rand = Math.floor(Math.random() * imgArr.length);
-  document.body.style.backgroundImage = "url(" + imgArr[rand] + ')';
-}
+var len = 5;
+var rand = Math.floor(Math.random() * len) + 1;
+document.body.style.backgroundImage = "url(/images/background/background" + String(rand) + '.webp)';
