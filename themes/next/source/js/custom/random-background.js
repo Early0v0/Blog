ab95d506a40b8c002bcff28129061311fd8bd092
Early <lin_erli@outlook.com>
2020-04-09 18:04:18 +0800

    OCPD! to Random-background

--- themes/next/source/js/custom/random-background.js
+++ themes/next/source/js/custom/random-background.js
@@ -1,3 +1,3 @@
 var len = 5;
 var rand = Math.floor(Math.random() * len) + 1;
-document.body.style.backgroundImage = "url(/images/background/background" + String(rand) + '.webp)';
+document.body.style.backgroundImage = "url(/images/background/background" + String(rand) + ".webp)";
